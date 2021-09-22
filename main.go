7e859e37eade0f79e1a7ddf415f41b07042027c0
Msksgm <msksgm3124@gmail.com>
2021-09-23 07:43:50 +0900

    add: TODOリストを表示する画面を追加

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate"
@@ -13,7 +15,15 @@ import (
 	_ "github.com/golang-migrate/migrate/source/file"
 )
 
+// HTMLファイルを embed するためのコード
+// go:embed index.html
+var f embed.FS
+
 var DB *sql.DB
+
+type ToDo struct {
+    Content string
+}
 
 // DBへの接続
 func connectDB() (*sql.DB, error) {
@@ -59,8 +69,39 @@ func migration() error {
     return nil
 }
 
-func RootHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello world! Welcome Go App!")
+func RootHandler(w http.ResponseWriter, r * http.Request) {
+    rows, err := DB.Query("select content from todo")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    todos := []ToDo{}
+    for rows.Next() {
+        var t ToDo
+        err := rows.Scan(&t.Content)
+        if err != nil {
+            log.Fatal(err)
+        }
+        todos = append(todos, t)
+    }
+
+    tmpl, _ := template.ParseFS(f, "index.html")
+    err = tmpl.Execute(w, todos)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+// Postの処理でToDoを登録
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+    content := r.FormValue("content")
+    rows, err := DB.Query(fmt.Sprintf("INSERT INTO tod (content) VALUES ('%s')", content))
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    http.Redirect(w, r, "/", 302)
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request)  {
@@ -87,6 +128,7 @@ func main() {
     switch os.Args[1] {
     case "start":
         http.HandleFunc("/", RootHandler)
+        http.HandleFunc("/todo", PostHandler)
         http.HandleFunc("/health_check", HealthCheck)
         log.Fatal(http.ListenAndServe(":8080", nil))
     case "migrate":
